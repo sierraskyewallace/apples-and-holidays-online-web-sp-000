f5ec85a62623cd1b73bb0ca5ea36700aff8f15e5
Sierra Wallace <sskwallace@gmail.com>
2020-08-22 23:58:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 holiday_hash[:winter].each do |holiday, value|
   value << supply
 end
-
+end
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
