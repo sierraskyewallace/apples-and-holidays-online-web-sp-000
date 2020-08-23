08ead82a5dca4c9ed53d9349f839841141ed0211
Sierra Wallace <sskwallace@gmail.com>
2020-08-23 00:05:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday|
-  puts "#{season.to_s.capitalize}:"
+  puts "#{season.to_s.capitalize}: "
   holiday.each do |holiday_event, supply|
   puts "#{holiday_event.to_s.split("_").each{|element| element.capitalize!}.join(" ")}: #{supply.join(",")}"
 end
