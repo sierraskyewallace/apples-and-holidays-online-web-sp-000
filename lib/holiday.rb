7a387f7c9898c50a8883ba56923cc0b708b8594b
Sierra Wallace <sskwallace@gmail.com>
2020-08-23 00:05:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts "#{season.to_s.capitalize}:"
   holiday.each do |holiday_event, supply|
-  puts "#{holiday_event.to_s.split("_").each{|element| element.capitalize!}.join(" ")}: #{supply.join(",")}"
+    "#{holiday_event.to_s.split("_").each{|element| element.capitalize!}.join(" ")}: #{supply.join(",")}"
 end
 end
 end
